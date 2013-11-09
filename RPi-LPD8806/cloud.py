21c8b2d9b956c6c6bc465914e34dca11b224b7a9
vcastane <vincent.castaneda@gnaul.com>
2013-11-09 21:42:03 +0000

    added some weahter stuff to the clou.py

--- RPi-LPD8806/cloud.py
+++ RPi-LPD8806/cloud.py
@@ -1,7 +1,20 @@
 from LPD8806 import *
 from animation import *
 import time
+import pywapi
+import string
 
+
+
+# Weather codes used for effects in the glowcloud
+rain = [1, 8, 9, 10, 11, 12, 40]
+thunderstorm = [3, 4, 37, 38, 39, 45, 47]
+snow = [5, 6, 7, 13, 14, 15, 16, 17, 18, 35, 41, 42, 43, 46]
+reallybad = [1, 2]
+badvisibility = [19, 20, 21, 22]
+windy = [23, 24]
+cloudy = [27, 28, 29, 30, 36, 44]
+noEffect = [25, 31, 32, 33, 34, 3200]
 
 def lightning(led, color='ffffff'):
   twinkle = FireFlies(led, [color_hex(color)], 30)
@@ -26,6 +39,7 @@ def pickGradientColor(temp):
   return code
 
 def pickColor(temp):
+  temp = int(float(temp))
   if (temp < -40):
     return '0000cc'
   elif temp < -30:
@@ -58,11 +72,28 @@ def fillStrip(led, color='ffffff'):
 def main():
   print pickColor(40)
 
-  led = LEDStrip(32, True)
-  
-  for i in range(-10,45):
-    fillStrip(led, pickGradientColor(i))
-    time.sleep(1)
+  led = LEDStrip(32, False)
+# lightning(led)  
+
+  while(True):
+    theWeather = pywapi.get_weather_from_yahoo('08544')
+    condition = theWeather['condition']['code']
+    temperature = theWeather['condition']['temp']
+
+    nextColor = pickColor(temperature)
+
+    fillStrip(led, nextColor)
+
+    print 'Condition Code; ' + condition
+    print 'Temp:' + temperature
+    print 'Color:' + nextColor
+
+    time.sleep(60)
+
+
+ # for i in range(-10,45):
+ #   fillStrip(led, pickGradientColor(i))
+ #   time.sleep(1)
   
 
 main()
