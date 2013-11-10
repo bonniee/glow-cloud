46af04d9b6e135e7020d63813c292a2cfb7692ee
Bonnie <bonnie.eisenman@gmail.com>
2013-11-09 21:27:46 -0500

    merged

--- RPi-LPD8806/cloud.py
+++ RPi-LPD8806/cloud.py
@@ -5,6 +5,8 @@ import pywapi
 import string
 import feedparser, pygame
 import pygame
+from datetime import datetime
+
 
 pygame.init()
 pygame.mixer.init()
@@ -29,6 +31,43 @@ cloudy = [27, 28, 29, 30, 36, 44]
 noEffect = [25, 31, 32, 33, 34, 3200]
 
 ANIMATION_LENGTH = 300
+
+
+def playUpdate():
+  allhail = pygame.mixer.Sound("sounds/allhailquote")
+  hello = pygame.mixer.Sound("sounds/hellonightvale")
+
+  allhail.play()
+  hello.play()
+  playTime()
+  playWeatherSound()
+
+def playNumber(num):
+  if (num < 0):
+    return
+  if (num < 20):
+    s = getNumberSound(num)
+    s.play()
+    return
+  else:
+    little = getNumberSound(num % 10)
+    big = getNumberSound(num / 10)
+    big.play()
+    little.play()
+    
+def playTime():
+  dt = datetime.now()
+  hour = dt.hour
+  minute = dt.minute
+  itis = pygame.mixer.Sound("sounds/itis.wav")
+  itis.play()
+  getNumberSound(hour).play()
+  if (minute == 0):
+    oclock = pygame.mixer.sound("sounds/oclock.wav")
+    oclock.play()
+  else:
+    playNumber(minute)
+
 
 def getNumberSound(i):
   fname = "sounds/" + str(i) + ".wav"
